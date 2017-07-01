4b02d27efd2af6ffdbfbd1d25bf9873a33687d69
Tatsuya Kojima <cy11121@shibaura-it.ac.jp>
2017-07-01 22:56:30 +0900

    詳細画面を作成

---
+++ ZoomInteractiveTransitionExample/ImageViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ImageViewController.swift
+//  ZoomInteractiveTransitionExample
+//
+//  Created by KojimaTatsuya on 2017/07/01.
+//  Copyright © 2017年 KojimaTatsuya. All rights reserved.
+//
+
+import UIKit
+
+class ImageViewController: UIViewController {
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
+}
